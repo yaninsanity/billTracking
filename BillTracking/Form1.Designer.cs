67fbf5982883efb3c4c50011e000ce8ddd0b1006
cribail <cribail@asu.edu>
2018-09-17 08:47:59 -0700

    Revert "Test"
    This reverts commit 0d25222cc24b2ea369d5ae092fcd6df92725c7db.

--- BillTracking/Form1.Designer.cs
+++ BillTracking/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(63, 135);
+            this.button1.Location = new System.Drawing.Point(59, 73);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(124, 34);
             this.button1.TabIndex = 0;
