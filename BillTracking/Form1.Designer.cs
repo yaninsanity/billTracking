7f05c8e0156964d8521a93b12088ddccc82d73d1
tmle9 <triet.le@asu.edu>
2018-09-18 12:55:59 -0700

    Put in Trevor button

--- BillTracking/Form1.Designer.cs
+++ BillTracking/Form1.Designer.cs
@@ -30,35 +30,47 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.trevorButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(118, 140);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.button1.Location = new System.Drawing.Point(79, 90);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(248, 65);
+            this.button1.Size = new System.Drawing.Size(165, 42);
             this.button1.TabIndex = 0;
             this.button1.Text = "Cameron";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(462, 140);
+            this.button2.Location = new System.Drawing.Point(308, 90);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(248, 65);
+            this.button2.Size = new System.Drawing.Size(165, 42);
             this.button2.TabIndex = 1;
             this.button2.Text = "Marco";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // trevorButton
+            // 
+            this.trevorButton.Location = new System.Drawing.Point(559, 90);
+            this.trevorButton.Name = "trevorButton";
+            this.trevorButton.Size = new System.Drawing.Size(150, 42);
+            this.trevorButton.TabIndex = 2;
+            this.trevorButton.Text = "Trevor";
+            this.trevorButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(2282, 1102);
+            this.ClientSize = new System.Drawing.Size(1276, 669);
+            this.Controls.Add(this.trevorButton);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -69,6 +81,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button trevorButton;
     }
 }
 
