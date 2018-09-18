bc106bfc17da430035fefb7d89a42cc34dd4a9fd
Marco Sampedro <msampedr@asu.edu>
2018-09-18 15:55:10 -0700

    Login and Sign up Form

---
+++ BillTracking/LoginSignupForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace BillTracking
+{
+    partial class LoginSignupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(523, 425);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "OR";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 402);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 71);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(669, 402);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 76);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Sign Up";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // LoginSignupForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1174, 629);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "LoginSignupForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
--- BillTracking/LoginSignupForm.cs
+++ BillTracking/LoginSignupForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace BillTracking
 {
-    public partial class Form1 : Form
+    public partial class LoginSignupForm : Form
     {
-        public Form1()
+        public LoginSignupForm()
         {
             InitializeComponent();
         }
