1fda79281d747024c3132bbdca48b9469cc291e8
tmle9 <triet.le@asu.edu>
2018-09-20 16:52:08 -0700

    Account

---
+++ BillTracking/Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillTracking
+{
+    class Account
+    {
+        private string username;
+        private string pin;
+        private string email;
+
+        public Account(string username, string pin, string email)
+        {
+            this.username = username;
+            this.pin = pin;
+            this.email = email;
+        }
+
+        public string Username { get => username; set => username = value; }
+        public string Pin { get => pin; set => pin = value; }
+        public string Email { get => email; set => email = value; }
+    }
+}
