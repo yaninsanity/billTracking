886f0bb1974c65b67b67fb627649039e19978eb2
cribail <cribail@asu.edu>
2018-09-20 17:03:22 -0700

    Created Bill Class

--- BillTracking/BillClass.cs
+++ BillTracking/BillClass.cs
@@ -8,5 +8,25 @@ namespace BillTracking
 {
     class BillClass
     {
-    }
+        string name;
+        string date;
+        double amount;
+        string[] recurrence = new string[] {"Daily", "Weekly", "Monthly", "Yearly" };
+        string[] day = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+        public BillClass(string name, string date, double amount, string[] recurrence, string[] day)
+        {
+            this.name = name;
+            this.date = date;
+            this.amount = amount;
+            this.recurrence = recurrence;
+            this.day = day;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public string Date { get => date; set => date = value; }
+        public double Amount { get => amount; set => amount = value; }
+        public string[] Recurrence { get => recurrence; }
+        public string[] Day { get => day; }
+    }  
 }
