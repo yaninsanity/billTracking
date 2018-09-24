95ce9f66889905188a0c46076b4b199c55d88c4b
tmle9 <triet.le@asu.edu>
2018-09-23 23:34:40 -0700

    Make the account class public and add the email textbox to the signup form

--- BillTracking/Account.cs
+++ BillTracking/Account.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BillTracking
 {
-    class Account
+    public class Account
     {
         private string username;
         private string pin;
--- BillTracking/SignUpForm.cs
+++ BillTracking/SignUpForm.cs
@@ -26,5 +26,34 @@ namespace BillTracking
             objLoginForm.Show();
             this.Hide();
         }
+
+        private void signUpButton_Click(object sender, EventArgs e)
+        {
+            //condition to test the username, pin and email not null
+            if (usernameTextBox.Text == String.Empty ||
+                pinTextBox.Text == String.Empty ||
+                emailTextBox.Text == String.Empty)
+            {
+                MessageBox.Show("Please enter all required fields");
+                return;
+            }
+
+            //condition to test the pin to be re-entered
+            if (reEnterPinTextBox.Text == String.Empty)
+            {
+                MessageBox.Show("Please re-enter you pin");
+                return;
+            }
+
+            //condition to test the matching of the pin and re-pin
+            if (reEnterPinTextBox.Text != pinTextBox.Text)
+            {
+                MessageBox.Show("Please re-enter your pin");
+                return;
+            }
+
+            //intantiate an account after all the info are entered
+            
+        }
     }
 }
