66c93f03879f6b03eddd6dfe47288332b5f1225f
Triet Le <tmle9@asu.edu>
2018-10-03 15:01:46 -0700

    Comment out the save/load function

--- BillTracking/HomePage.cs
+++ BillTracking/HomePage.cs
@@ -28,10 +28,10 @@ namespace BillTracking
             if (result == DialogResult.Yes)
             {
                 //This is where saving needs to be implemented.
-                FileStream file = new FileStream("data.dat", FileMode.Create, FileAccess.Write);
-                BinaryFormatter bf = new BinaryFormatter();
-                bf.Serialize(file, BillMasterList);
-                file.Close();
+                //FileStream file = new FileStream("data.dat", FileMode.Create, FileAccess.Write);
+                //BinaryFormatter bf = new BinaryFormatter();
+                //bf.Serialize(file, BillMasterList);
+                //file.Close();
 
                 //Open back the loggin form
                 LoginForm LoginForm = new LoginForm();
@@ -51,11 +51,11 @@ namespace BillTracking
 
         private void SetEventHandlers_Load(object sender, EventArgs e)
         {
-            //load from dat file
-            FileStream file = new FileStream("data.dat", FileMode.Open, FileAccess.Read);
-            BinaryFormatter bf = new BinaryFormatter();
-            BillMasterList = (BindingList<Bill>)bf.Deserialize(file);
-            file.Close();
+            ////load from dat file
+            //FileStream file = new FileStream("data.dat", FileMode.Open, FileAccess.Read);
+            //BinaryFormatter bf = new BinaryFormatter();
+            //BillMasterList = (BindingList<Bill>)bf.Deserialize(file);
+            //file.Close();
 
 
             BillForm = new BillForm(BillMasterList);
