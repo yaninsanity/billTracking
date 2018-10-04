9df0424e0c171c6aeb5b8ece7db0089a62388a17
Marco Sampedro <msampedr@asu.edu>
2018-10-03 17:39:15 -0700

    Minor changes

--- BillTracking/LoginSignupForm.Designer.cs
+++ BillTracking/LoginSignupForm.Designer.cs
@@ -40,10 +40,10 @@
             // 
             this.orLabel.AutoSize = true;
             this.orLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.orLabel.Location = new System.Drawing.Point(163, 200);
-            this.orLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.orLabel.Location = new System.Drawing.Point(326, 385);
+            this.orLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.orLabel.Name = "orLabel";
-            this.orLabel.Size = new System.Drawing.Size(33, 18);
+            this.orLabel.Size = new System.Drawing.Size(61, 36);
             this.orLabel.TabIndex = 0;
             this.orLabel.Text = "OR";
             // 
@@ -52,10 +52,10 @@
             this.loginButton.BackColor = System.Drawing.Color.RoyalBlue;
             this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.loginButton.ForeColor = System.Drawing.Color.White;
-            this.loginButton.Location = new System.Drawing.Point(125, 133);
-            this.loginButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.loginButton.Location = new System.Drawing.Point(250, 256);
+            this.loginButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.loginButton.Name = "loginButton";
-            this.loginButton.Size = new System.Drawing.Size(108, 37);
+            this.loginButton.Size = new System.Drawing.Size(216, 71);
             this.loginButton.TabIndex = 1;
             this.loginButton.Text = "Login";
             this.loginButton.UseVisualStyleBackColor = false;
@@ -66,10 +66,10 @@
             this.signUpButton.BackColor = System.Drawing.Color.RoyalBlue;
             this.signUpButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.signUpButton.ForeColor = System.Drawing.Color.White;
-            this.signUpButton.Location = new System.Drawing.Point(125, 252);
-            this.signUpButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.signUpButton.Location = new System.Drawing.Point(250, 485);
+            this.signUpButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.signUpButton.Name = "signUpButton";
-            this.signUpButton.Size = new System.Drawing.Size(108, 40);
+            this.signUpButton.Size = new System.Drawing.Size(216, 77);
             this.signUpButton.TabIndex = 2;
             this.signUpButton.Text = "Sign Up";
             this.signUpButton.UseVisualStyleBackColor = false;
@@ -79,21 +79,21 @@
             // 
             this.appTitleLabel.AutoSize = true;
             this.appTitleLabel.Font = new System.Drawing.Font("Eras Bold ITC", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.appTitleLabel.Location = new System.Drawing.Point(82, 32);
-            this.appTitleLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.appTitleLabel.Location = new System.Drawing.Point(164, 62);
+            this.appTitleLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.appTitleLabel.Name = "appTitleLabel";
-            this.appTitleLabel.Size = new System.Drawing.Size(207, 40);
+            this.appTitleLabel.Size = new System.Drawing.Size(431, 78);
             this.appTitleLabel.TabIndex = 3;
-            this.appTitleLabel.Text = "App Name";
+            this.appTitleLabel.Text = "Bill Tracker";
             // 
             // dashLineLabel1
             // 
             this.dashLineLabel1.AutoSize = true;
             this.dashLineLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dashLineLabel1.Location = new System.Drawing.Point(104, 195);
-            this.dashLineLabel1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.dashLineLabel1.Location = new System.Drawing.Point(208, 375);
+            this.dashLineLabel1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.dashLineLabel1.Name = "dashLineLabel1";
-            this.dashLineLabel1.Size = new System.Drawing.Size(63, 15);
+            this.dashLineLabel1.Size = new System.Drawing.Size(111, 29);
             this.dashLineLabel1.TabIndex = 4;
             this.dashLineLabel1.Text = "_______";
             // 
@@ -101,27 +101,28 @@
             // 
             this.dashLineLabel2.AutoSize = true;
             this.dashLineLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dashLineLabel2.Location = new System.Drawing.Point(197, 195);
-            this.dashLineLabel2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.dashLineLabel2.Location = new System.Drawing.Point(394, 375);
+            this.dashLineLabel2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.dashLineLabel2.Name = "dashLineLabel2";
-            this.dashLineLabel2.Size = new System.Drawing.Size(63, 15);
+            this.dashLineLabel2.Size = new System.Drawing.Size(111, 29);
             this.dashLineLabel2.TabIndex = 5;
             this.dashLineLabel2.Text = "_______";
             // 
             // LoginSignupForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(369, 372);
+            this.ClientSize = new System.Drawing.Size(738, 715);
             this.Controls.Add(this.dashLineLabel2);
             this.Controls.Add(this.dashLineLabel1);
             this.Controls.Add(this.appTitleLabel);
             this.Controls.Add(this.signUpButton);
             this.Controls.Add(this.loginButton);
             this.Controls.Add(this.orLabel);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "LoginSignupForm";
-            this.Text = "*APP NAME*";
+            this.Text = "Bill Tracker";
             this.ResumeLayout(false);
             this.PerformLayout();
 
